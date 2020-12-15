195c8daab14eca825ec03327eb3188c3d380fa58
K Gebski <kgebski68@gmail.com>
2020-12-15 20:09:56 +0100

    Shortened ifs. Register name, sellIn and quality to vaiables

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -6,12 +6,25 @@ GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
 
 GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 {}
+ 
 
 void GildedRose::updateQuality()
 {
+    std::string name;
+    int quality, sellIn;
+
+    std::string aged = "Aged Brie";
+    std::string sulfuras = "Sulfuras, Hand of Ragnaros";
+    std::string backstage = "Backstage passes to a TAFKAL80ETC concert";
+    
+
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" && items[i].name != "Sulfuras, Hand of Ragnaros")
+        name = items[i].name;
+        quality = items[i].quality;
+        sellIn = items[i].sellIn;
+
+        if (name != aged && name != backstage && name != sulfuras)
         {
             if (items[i].quality > 0)
             {
@@ -24,7 +37,7 @@ void GildedRose::updateQuality()
             {
                 ++items[i].quality;
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+                if (name == backstage)
                 {
                     if (items[i].sellIn < 11)
                     {
@@ -39,20 +52,20 @@ void GildedRose::updateQuality()
             }
         }
 
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+        if (name != sulfuras)
         {
             --items[i].sellIn;
         }
 
         if (items[i].sellIn < 0)
         {
-            if (items[i].name != "Aged Brie")
+            if (name != aged)
             {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                if (name != backstage)
                 {
                     if (items[i].quality > 0)
                     {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                        if (name != sulfuras)
                         {
                             --items[i].quality;
                         }
