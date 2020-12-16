2e0aef98203decb8b419f50330ea63fa9c4f1a5b
jarek69 <roszyk.jarek@gmail.com>
2020-12-16 03:29:07 +0100

    Change if statements

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -1,17 +1,20 @@
 #include "GildedRose.h"
 //create variables holding words
 //maybe create function increment and decrementation
-GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
-{}
+GildedRose::GildedRose(::std::vector<Item> const &items) : items(items)
+{
+}
 
-GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
-{}
+GildedRose::GildedRose(::std::vector<Item> &&items) : items(::std::move(items))
+{
+}
 
 void GildedRose::updateQuality()
 {
-    std::string nameOne="Aged Brie";
-    std::string nameTwo="Backstage passes to a TAFKAL80ETC concert";
-    std::string nameThree="Sulfuras, Hand of Ragnaros";
+    std::string nameOne = "Aged Brie";
+    std::string nameTwo = "Backstage passes to a TAFKAL80ETC concert";
+    std::string nameThree = "Sulfuras, Hand of Ragnaros";
+
     for (int i = 0; i < items.size(); i++)
     {
         if (items[i].name != nameOne && items[i].name != nameTwo && items[i].name != nameThree)
@@ -31,7 +34,7 @@ void GildedRose::updateQuality()
                 {
                     if (items[i].sellIn < 11)
                     {
-                        ++items[i].quality; 
+                        ++items[i].quality;
                     }
 
                     if (items[i].sellIn < 6)
@@ -49,27 +52,42 @@ void GildedRose::updateQuality()
 
         if (items[i].sellIn < 0)
         {
-            if (items[i].name != nameOne)
+            // if (items[i].name != nameOne)
+            // {
+            //     if (items[i].name != nameTwo)
+            //     {
+            //         if (items[i].quality > 0)
+            //         {
+            //             if (items[i].name != nameThree)
+            //             {
+            //                 --items[i].quality;
+            //             }
+            //         }
+            //     }
+            //     else
+            //     {
+            //         items[i].quality = 0;
+            //     }
+            // }
+            if (items[i].name != nameOne && items[i].name != nameTwo && items[i].name != nameThree)
             {
-                if (items[i].name != nameTwo)
+                if (items[i].quality > 0)
                 {
-                    if (items[i].quality > 0)
-                    {
-                        if (items[i].name != nameThree)
-                        {
-                            --items[i].quality;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality=0;
+                    --items[i].quality;
                 }
             }
-            else if (items[i].quality < 50)
+            if (items[i].name == nameTwo)
+            {
+                items[i].quality = 0;
+            }
+            if (items[i].quality < 50 && items[i].name == nameOne)
             {
                 ++items[i].quality;
             }
+            // else if (items[i].quality < 50)
+            // {
+            //     ++items[i].quality;
+            // }
         }
     }
 }
