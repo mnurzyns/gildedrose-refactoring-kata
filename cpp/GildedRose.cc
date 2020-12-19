acb2f007e1ea6dca4b9aaeb16d87818f5d9b7ce9
Jacek Jarosz <jacekjarosz55@gmail.com>
2020-12-19 18:33:20 +0100

    Refactored GildedRose::updateQuality() method by shortening the code to three main if statements.

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -9,68 +9,92 @@ GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 
 void GildedRose::updateQuality()
 {
-    const std::string AGEDBRIE = "Aged Brie";
-    const std::string BACKSTAGEPASS = "Backstage passes to a TAFKAL80ETC concert";
-    const std::string SULFURAS = "Sulfuras, Hand of Ragnaros";
-    
-    for (int i = 0; i < items.size(); i++)
-    {
-        if (items[i].name != AGEDBRIE && items[i].name != BACKSTAGEPASS && items[i].name != SULFURAS)
+    const char* AGEDBRIE = "Aged Brie";
+    const char* BACKSTAGEPASS = "Backstage passes to a TAFKAL80ETC concert";
+    const char* SULFURAS = "Sulfuras, Hand of Ragnaros";
+  
+        // --- refactored code --- // 
+for (int i = 0; i < items.size(); i++)
+{
+
+        
+        if(items[i].name == AGEDBRIE)
+        {
+            if (items[i].quality < 50)
+            {
+                ++items[i].quality;
+            }
+            
+            if(items[i].name != SULFURAS)
+            {
+                --items[i].sellIn;
+            }
+            
+            if (items[i].sellIn < 0)
+            {
+                if (items[i].quality < 50)
+                {
+                    ++items[i].quality;
+                }
+            }
+        }
+        else if (items[i].name == BACKSTAGEPASS)
+        {
+            if (items[i].quality < 50)
+            {
+                ++items[i].quality;
+                
+                if (items[i].sellIn < 6)
+                {
+                    ++items[i].quality;
+                }
+                if (items[i].sellIn < 11)
+                {
+                    ++items[i].quality; 
+                }
+            }
+            if(items[i].name != SULFURAS)
+            {
+            --items[i].sellIn;
+            }
+            
+            if (items[i].sellIn < 0)
+            {
+                items[i].quality=0;
+            }
+        }
+        
+        else if (items[i].name == SULFURAS)
+        {
+            if (items[i].quality < 50)
+            {
+                ++items[i].quality;
+            }
+        }
+        
+        else
         {
             if (items[i].quality > 0)
             {
                 --items[i].quality;
             }
-        }
-        else
-        {
-            if (items[i].quality < 50)
+            
+            if(items[i].name != SULFURAS)
             {
-                ++items[i].quality;
-
-                if (items[i].name == BACKSTAGEPASS)
+                --items[i].sellIn;
+            }
+            
+            if (items[i].sellIn < 0)
+            {
+                if (items[i].quality > 0)
                 {
-                    if (items[i].sellIn < 11)
-                    {
-                        ++items[i].quality; 
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                        ++items[i].quality;
-                    }
+                    --items[i].quality;
                 }
             }
-        }
-
-        if (items[i].name != SULFURAS)
-        {
-            --items[i].sellIn;
-        }
-
-        if (items[i].sellIn < 0)
-        {
-            if (items[i].name != AGEDBRIE)
-            {
-                if (items[i].name != BACKSTAGEPASS)
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (items[i].name != SULFURAS)
-                        {
-                            --items[i].quality;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality=0;
-                }
-            }
-            else if (items[i].quality < 50)
-            {
-                ++items[i].quality;
-            }
+            
         }
     }
 }
+
+
+
