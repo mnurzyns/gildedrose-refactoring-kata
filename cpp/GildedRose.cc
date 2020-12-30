82596701b051cacccf7e20122ba95569aef1698e
ankiedos <antek_kiedos@outlook.com>
2020-12-30 16:43:03 +0100

    Linked if statements and removed braces in one line ifs.

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -11,74 +11,27 @@ void GildedRose::updateQuality()
 {
     for (auto elem : items)
     {
-        if (elem.name != "Aged Brie" && elem.name != "Backstage passes to a TAFKAL80ETC concert")
+        if (elem.name != "Aged Brie" && elem.name != "Backstage passes to a TAFKAL80ETC concert" && elem.quality > 0 && elem.name != "Sulfuras, Hand of Ragnaros")
+            --elem.quality;
+        else if (elem.quality < 50)
         {
-            if (elem.quality > 0)
-            {
-                if (elem.name != "Sulfuras, Hand of Ragnaros")
-                {
-                    --elem.quality;
-                }
-            }
-        }
-        else
-        {
-            if (elem.quality < 50)
-            {
-                ++elem.quality;
+            ++elem.quality;
 
-                if (elem.name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (elem.sellIn < 11)
-                    {
-                        if (elem.quality < 50)
-                        {
-                            ++elem.quality;
-                        }
-                    }
-
-                    if (elem.sellIn < 6)
-                    {
-                        if (elem.quality < 50)
-                        {
-                            ++elem.quality;
-                        }
-                    }
-                }
-            }
+            if ((elem.name == "Backstage passes to a TAFKAL80ETC concert" && (elem.sellIn < 11 && elem.quality < 50)) ||
+                (elem.name == "Backstage passes to a TAFKAL80ETC concert" && (elem.sellIn < 11 && elem.quality < 50)))
+                    ++elem.quality;
         }
 
-        if (elem.name != "Sulfuras, Hand of Ragnaros")
-        {
-            --elem.sellIn;
-        }
+        if (elem.name != "Sulfuras, Hand of Ragnaros") --elem.sellIn;
 
         if (elem.sellIn < 0)
         {
             if (elem.name != "Aged Brie")
             {
-                if (elem.name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (elem.quality > 0)
-                    {
-                        if (elem.name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            --elem.quality;
-                        }
-                    }
-                }
-                else
-                {
-                    elem.quality = 0;
-                }
+                if (elem.name != "Backstage passes to a TAFKAL80ETC concert" && elem.quality > 0 && elem.name != "Sulfuras, Hand of Ragnaros") --elem.quality;
+                else elem.quality = 0;
             }
-            else
-            {
-                if (elem.quality < 50)
-                {
-                    ++elem.quality;
-                }
-            }
+            else if (elem.quality < 50) ++elem.quality;
         }
     }
 }
