54541f02875c8aafeab13d2b797887496d94e49d
ankiedos <antek_kiedos@outlook.com>
2020-12-30 21:19:05 +0100

    Removed spaces between 'if' and '(' as well as  between 'for' and '('.

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -13,25 +13,25 @@ void GildedRose::updateQuality()
     const string brie = "Aged Brie";
     const string concert = "Backstage passes to a TAFKAL80ETC concert";
     const string sulfuras = "Sulfuras, Hand of Ragnaros";
-    for (auto elem : items)
+    for(auto elem : items)
     {
-        if (elem.name != brie && elem.name != concert && elem.name != sulfuras && elem.quality > 0)
+        if(elem.name != brie && elem.name != concert && elem.name != sulfuras && elem.quality > 0)
             --elem.quality;
-        else if (elem.quality < 50)
+        else if(elem.quality < 50)
         {
             ++elem.quality;
 
-            if (elem.name == concert && elem.sellIn < 6) ++elem.quality;
-            if (elem.name == concert && elem.sellIn < 11) ++elem.quality;
+            if(elem.name == concert && elem.sellIn < 6) ++elem.quality;
+            if(elem.name == concert && elem.sellIn < 11) ++elem.quality;
         }
 
-        if (elem.name != sulfuras) --elem.sellIn;
+        if(elem.name != sulfuras) --elem.sellIn;
 
-        if (elem.sellIn < 0)
+        if(elem.sellIn < 0)
         {
-            if (elem.name != brie && elem.name != concert && elem.name != sulfuras && elem.quality > 0) --elem.quality;
-            else if (elem.name != brie  && (elem.name == concert || elem.name == sulfuras || elem.quality <= 0)) elem.quality = 0;
-            else if (elem.quality < 50) ++elem.quality;
+            if(elem.name != brie && elem.name != concert && elem.name != sulfuras && elem.quality > 0) --elem.quality;
+            else if(elem.name != brie  && (elem.name == concert || elem.name == sulfuras || elem.quality <= 0)) elem.quality = 0;
+            else if(elem.quality < 50) ++elem.quality;
         }
     }
 }
