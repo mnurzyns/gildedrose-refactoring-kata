0e5b3398ab703ed3d13f112c372d6667ad2cd5fc
ankiedos <antek_kiedos@outlook.com>
2020-12-31 15:05:42 +0100

    Finishing works.

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -13,10 +13,10 @@ void GildedRose::updateQuality()
     const string brie = "Aged Brie";
     const string concert = "Backstage passes to a TAFKAL80ETC concert";
     const string sulfuras = "Sulfuras, Hand of Ragnaros";
+    
     for(int i = 0; i < items.size(); i++)
     {
-        if(items[i].isRegularItem && items[i].quality > 0)
-            --items[i].quality;
+        if     ( items[i].isRegularItem && items[i].quality > 0) --items[i].quality;
         else if(!items[i].isRegularItem && items[i].quality < 50)
         {
             ++items[i].quality;
@@ -29,9 +29,10 @@ void GildedRose::updateQuality()
 
         if(items[i].sellIn < 0)
         {
-            if(items[i].isRegularItem && items[i].quality > 0) --items[i].quality;
-            else if(items[i].name != brie  && (items[i].name == concert || items[i].name == sulfuras || items[i].quality <= 0)) items[i].quality = 0;
-            else if(items[i].name == brie && items[i].quality < 50) ++items[i].quality;
+            if     (items[i].isRegularItem && items[i].quality > 0) --items[i].quality;
+            else if(items[i].name != brie  &&(items[i].name == concert || items[i].name
+                == sulfuras || items[i].quality <= 0)) items[i].quality = 0;
+            else if(items[i].name == brie  && items[i].quality < 50) ++items[i].quality;
         }
     }
 }
