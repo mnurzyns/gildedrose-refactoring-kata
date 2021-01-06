f4ddfc873fde563b6808f5ec63c8316d91d7e45e
kHuryn <zasilaczv@gmail.com>
2021-01-06 12:24:29 -0800

    added variables which holds strings values

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -1,94 +1,63 @@
 #include "GildedRose.h"
 
 
-GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
-{}
+GildedRose::GildedRose(::std::vector < Item >
+  const & items): items(items) {}
 
-GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
-{}
+GildedRose::GildedRose(::std::vector < Item > && items): items(::std::move(items)) {}
 
-void GildedRose::updateQuality()
-{
-    for (int i = 0; i < items.size(); i++)
-    {
+void GildedRose::updateQuality() {
+  std::string x = "Backstage passes to a TAFKAL80ETC concert";
+  std::string y = "Sulfuras, Hand of Ragnaros";
+  std::string z = "Aged Brie";
+  for (int i = 0; i < items.size(); i++) {
 
+    if (items[i].quality > 0) {
 
-        if(items[i].quality > 0) {
-
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                {
-                    items[i].quality--;
-                }  
+      if (items[i].name != z && items[i].name != x) {
+        if (items[i].name != y) {
+          items[i].quality--;
         }
+      } else {
+        if (items[i].quality < 50) {
+          items[i].quality++;
 
+          if (items[i].name == x) {
+            if (items[i].sellIn < 11) {
 
+              items[i].quality++;
 
-        else
-        {
-            if (items[i].quality < 50)
-            {
-                items[i].quality++;
-
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].sellIn < 11)
-                    {
-
-                            items[i].quality++;
-                        
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                            items[i].quality++;
-                    }
-                }
             }
-        }
 
-
-        }
-
-
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-        {
-            items[i].sellIn--;
-        }
-
-
-
-
-        if (items[i].sellIn < 0)
-        {
-            if (items[i].name != "Aged Brie")
-            {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            items[i].quality--;
-                        }
-                    }
-                }
-                else
-                {
-                    items[i].quality = 0;
-                }
+            if (items[i].sellIn < 6) {
+              items[i].quality++;
             }
-            else
-            {
-                if (items[i].quality < 50)
-                {
-                    items[i].quality++;
-                }
-            }
+          }
         }
-
-
+      }
 
     }
+    if (items[i].name != y) {
+      items[i].sellIn--;
+    }
+
+    if (items[i].sellIn < 0) {
+      if (items[i].name != z) {
+        if (items[i].name != x) {
+          if (items[i].quality > 0 && items[i].name != y) {
+
+            items[i].quality--;
+
+          }
+        } else {
+          items[i].quality = 0;
+        }
+      } else {
+        if (items[i].quality < 50) {
+          items[i].quality++;
+        }
+      }
+    }
+
+  }
 }
