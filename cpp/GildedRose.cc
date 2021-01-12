2a3daf2ed6b9b47186037f18694e368c5646560c
Jacek Jarosz <jacekjarosz55@gmail.com>
2021-01-12 18:30:13 +0100

    Added refreshQuality function. SellIn is subtracted from each item now, and added only for Sulfuras, the Hand of Ragnaros.

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -1,90 +1,59 @@
 #include "GildedRose.h"
 
+GildedRose::GildedRose(::std::vector<Item> const& items)
+    : items(items) {
+}
 
-GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
-{}
+GildedRose::GildedRose(::std::vector<Item>&& items)
+    : items(::std::move(items)) {
+}
 
-GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
-{}
+// jeżeli quality < 50 to dodaj quality
+int refreshQuality(int& itquality) {
+    if (itquality < 50)
+        itquality++;
+    return itquality;
+}
 
-void GildedRose::updateQuality()
-{
+void GildedRose::updateQuality() {
     const char* AGEDBRIE = "Aged Brie";
     const char* BACKSTAGEPASS = "Backstage passes to a TAFKAL80ETC concert";
     const char* SULFURAS = "Sulfuras, Hand of Ragnaros";
-  
-        // --- refactored code --- // 
-for (int i = 0; i < items.size(); i++)
-{
-        if(items[i].name == AGEDBRIE)
-        {
-            if (items[i].quality < 50)
-            {
+
+    for (int i = 0; i < items.size(); i++) {
+        --items[i].sellIn;
+        if (items[i].name == AGEDBRIE) {
+            refreshQuality(items[i].quality);
+            if (items[i].sellIn < 0) {
+                refreshQuality(items[i].quality);
+            }
+        } else if (items[i].name == BACKSTAGEPASS) {
+            refreshQuality(items[i].quality);
+
+            if (items[i].sellIn < 5) {
                 ++items[i].quality;
             }
-            
-                --items[i].sellIn;
-            
-            if (items[i].sellIn < 0)
-            {
-                if (items[i].quality < 50)
-                {
-                    ++items[i].quality;
-                }
-            }
-        }
-        else if (items[i].name == BACKSTAGEPASS)
-        {
-            if (items[i].quality < 50)
-            {
-                ++items[i].quality;
-                
-                if (items[i].sellIn < 6)
-                {
-                    ++items[i].quality;
-                }
-                if (items[i].sellIn < 11)
-                {
-                    ++items[i].quality; 
-                }
-            }
-            
-            --items[i].sellIn;            
-            
-            if (items[i].sellIn < 0)
-            {
-                items[i].quality=0;
-            }
-        }
-        
-        else if (items[i].name == SULFURAS)
-        {
-            if (items[i].quality < 50)
-            {
+            if (items[i].sellIn < 10) {
                 ++items[i].quality;
             }
+            if (items[i].sellIn < 0) {
+                items[i].quality = 0;
+            }
+        } else if (items[i].name == SULFURAS) {
+            items[i].sellIn++;
+            refreshQuality(items[i].quality);
         }
-        
-        else // Every other item
+
+        else  // Every other item
         {
-            if (items[i].quality > 0)
-            {
+            if (items[i].quality > 0) {
                 --items[i].quality;
             }
-            
-                --items[i].sellIn;
-            
-            if (items[i].sellIn < 0)
-            {
-                if (items[i].quality > 0)
-                {
+            if (items[i].sellIn < 0) {
+                if (items[i].quality > 0) {
                     --items[i].quality;
                 }
             }
-            
         }
     }
 }
-
-
-
