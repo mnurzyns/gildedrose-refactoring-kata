11554053730016bb112c2aa350bf21eeb27ce50d
LukaszKowalShuoShi <ti2a.kowal.lukasz@gmail.com>
2021-01-16 22:42:46 +0100

    Full refactorisation of the file. We do not have to check the sulfuras more than one time in the updating function. Added a few IF ELSE statements so that wo do not check every condition - only the ones that are crucial. Moreover, the file is shorter

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -15,56 +15,33 @@ void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != aged && items[i].name != backstage && items[i].name != sulfuras)
+        if (items[i].name != sulfuras) // for items that change their quality; Sulfuras doesn't
         {
-            if (items[i].quality > 0) items[i].quality -= 1;        
-        }
-        else
-        {
-            if (items[i].quality < 50)
+            if(items[i].sellIn < 0 || items[i].sellIn == 0) // when dealing with "changable" elements and sellIn < 0 or == 0
             {
-                items[i].quality += 1;
-
-                if (items[i].name == backstage)
-                {
-                    if (items[i].sellIn < 11)
-                    {
-                        if (items[i].quality < 50)
-                            items[i].quality += 1;
-                    }
-
-                    if (items[i].sellIn < 6)
-                    {
-                        if (items[i].quality < 50)
-                            items[i].quality += 1;
-                    }
+                if(items[i].name==aged)
+                { 
+                    items[i].quality += 2; items[i].sellIn -= 1; 
+                    if(items[i].quality > 50) { items[i].quality = 50; }
                 }
+                else if(items[i].name==backstage)
+                { items[i].quality = 0; items[i].sellIn -= 1; }
+                else { items[i].sellIn -=1; items[i].quality -=2; }
             }
-        }
-
-        if (items[i].name != sulfuras)
-            items[i].sellIn -= 1;
-
-        if (items[i].sellIn < 0)
-        {
-            if (items[i].name != aged)
+            else if (items[i].quality > 0 && items[i].quality < 50) // quality in range (0,50)
             {
-                if (items[i].name != backstage)
-                {
-                    if (items[i].quality > 0)
-                    {
-                        if (items[i].name != sulfuras)
-                            items[i].quality -= 1;
-                    }
+                if(items[i].name == aged) { items[i].sellIn -= 1; items[i].quality += 1; }
+                else if(items[i].name == backstage)
+                { 
+                    if(items[i].sellIn < 1) { items[i].quality = 0; items[i].sellIn -= 1; } // if less than 1 (0 and less) - after concert
+                    else if(items[i].sellIn < 6 && items[i].quality+3 < 51 ) { items[i].quality +=3; items[i].sellIn -=1; }
+                    else if(items[i].sellIn < 11 && items[i].quality+2 < 51 ) { items[i].quality +=2; items[i].sellIn -=1; }
                 }
-                else
-                    items[i].quality -= items[i].quality;
+                else { items[i].quality -=1; items[i].sellIn -=1; } // for regular items
             }
-            else
-            {
-                if (items[i].quality < 50)
-                    items[i].quality += 1;
-            }
+            else if(items[i].quality == 0 || items[i].quality == 50 )
+                if(items[i].name == aged || items[i].name != backstage) // aged brie and regular ones; do not change the quality
+                    items[i].sellIn -=1; 
         }
     }
 }
