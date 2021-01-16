6d10281344cc985b7502e526d7b1fdb844d3869b
LukaszKowalShuoShi <ti2a.kowal.lukasz@gmail.com>
2021-01-16 22:25:09 +0100

    deleted superfluous parentheses

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -13,10 +13,7 @@ void GildedRose::updateQuality()
     {
         if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" && items[i].name != "Sulfuras, Hand of Ragnaros")
         {
-            if (items[i].quality > 0)
-            {
-                    items[i].quality -= 1;
-            }
+            if (items[i].quality > 0) items[i].quality -= 1;        
         }
         else
         {
@@ -29,26 +26,20 @@ void GildedRose::updateQuality()
                     if (items[i].sellIn < 11)
                     {
                         if (items[i].quality < 50)
-                        {
                             items[i].quality += 1;
-                        }
                     }
 
                     if (items[i].sellIn < 6)
                     {
                         if (items[i].quality < 50)
-                        {
                             items[i].quality += 1;
-                        }
                     }
                 }
             }
         }
 
         if (items[i].name != "Sulfuras, Hand of Ragnaros")
-        {
             items[i].sellIn -= 1;
-        }
 
         if (items[i].sellIn < 0)
         {
@@ -59,22 +50,16 @@ void GildedRose::updateQuality()
                     if (items[i].quality > 0)
                     {
                         if (items[i].name != "Sulfuras, Hand of Ragnaros")
-                        {
                             items[i].quality -= 1;
-                        }
                     }
                 }
                 else
-                {
                     items[i].quality -= items[i].quality;
-                }
             }
             else
             {
                 if (items[i].quality < 50)
-                {
                     items[i].quality += 1;
-                }
             }
         }
     }
