e0ec3b54048877941bde67fe66a266d11de96d96
LukaszKowalShuoShi <ti2a.kowal.lukasz@gmail.com>
2021-01-16 22:33:15 +0100

    names of cards are stored in variables

--- cpp/GildedRose.cc
+++ cpp/GildedRose.cc
@@ -7,11 +7,15 @@ GildedRose::GildedRose(::std::vector<Item> const& items) : items(items)
 GildedRose::GildedRose(::std::vector<Item> && items) : items(::std::move(items))
 {}
 
+static std::string aged = "Aged Brie";
+static std::string backstage = "Backstage passes to a TAFKAL80ETC concert";
+static std::string sulfuras = "Sulfuras, Hand of Ragnaros";
+
 void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" && items[i].name != "Sulfuras, Hand of Ragnaros")
+        if (items[i].name != aged && items[i].name != backstage && items[i].name != sulfuras)
         {
             if (items[i].quality > 0) items[i].quality -= 1;        
         }
@@ -21,7 +25,7 @@ void GildedRose::updateQuality()
             {
                 items[i].quality += 1;
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].name == backstage)
                 {
                     if (items[i].sellIn < 11)
                     {
@@ -38,18 +42,18 @@ void GildedRose::updateQuality()
             }
         }
 
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+        if (items[i].name != sulfuras)
             items[i].sellIn -= 1;
 
         if (items[i].sellIn < 0)
         {
-            if (items[i].name != "Aged Brie")
+            if (items[i].name != aged)
             {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].name != backstage)
                 {
                     if (items[i].quality > 0)
                     {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                        if (items[i].name != sulfuras)
                             items[i].quality -= 1;
                     }
                 }
